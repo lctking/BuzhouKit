197b42a4144fe70b1ced28bc0898cf2c9a0d4743
lctking <1713673102lw@gmail.com>
2024-12-30 18:26:32 +0800

    init project

---
+++ src/main/java/com/lctking/buzhoukit/BuZhouKitApplication.java
@@ -0,0 +1,13 @@
+package com.lctking.buzhoukit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuZhouKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuZhouKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lctking/buzhoukit/BuZhouKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lctking.buzhoukit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuZhouKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

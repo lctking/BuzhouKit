e92bba726aaa8117fd64cd2b9e41363bb797dc49
lctking <1713673102lw@gmail.com>
2024-12-30 18:50:18 +0800

    init buzhoukit-core

---
+++ BuZhouKit-core/src/main/java/com/lctking/buzhoukitcore/BuzhouKitCoreApplication.java
@@ -0,0 +1,13 @@
+package com.lctking.buzhoukitcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzhouKitCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuzhouKitCoreApplication.class, args);
+    }
+
+}
---
+++ BuZhouKit-core/src/test/java/com/lctking/buzhoukitcore/BuzhouKitCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lctking.buzhoukitcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuzhouKitCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

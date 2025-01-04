bbf58203d05b4ad037e4bd43525e4e4e10cc9367
lctking <1713673102lw@gmail.com>
2025-01-04 22:56:42 +0800

    增加：幂等组件真实web测试相关

---
+++ Test/src/test/java/com/lctking/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lctking.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

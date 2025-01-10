3740b2a73e82c5bd7885a07045472eccf88c320c
lctking <1713673102lw@gmail.com>
2025-01-11 00:08:16 +0800

    更新：在总自动配置类中添加布隆过滤器的配置方法

--- BuZhouKit-Idempotent/src/main/java/com/lctking/buzhoukitidempotent/config/IdempotentAutoConfiguration.java
+++ BuZhouKit-Idempotent/src/main/java/com/lctking/buzhoukitidempotent/config/IdempotentAutoConfiguration.java
@@ -4,12 +4,16 @@ import com.lctking.buzhoukitidempotent.aspect.IdempotentAspect;
 import com.lctking.buzhoukitidempotent.cache.impl.CaffeineCacheServiceImpl;
 import com.lctking.buzhoukitidempotent.cache.impl.RedisCacheServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
-
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(value = {BloomFilterCacheProperties.class})
 public class IdempotentAutoConfiguration {
     private final StringRedisTemplate stringRedisTemplate;
 
@@ -26,5 +30,13 @@ public class IdempotentAutoConfiguration {
     @Bean
     CaffeineCacheServiceImpl<Object, Object> CaffeineCacheServiceImpl(){
         return new CaffeineCacheServiceImpl<>();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = BloomFilterCacheProperties.PREFIX, name = "enabled" ,havingValue = "true")
+    public RBloomFilter<String> BloomFilterCacheServiceImpl(RedissonClient redissonClient, BloomFilterCacheProperties bloomFilterCacheProperties){
+        RBloomFilter<String> bloomFilter = redissonClient.getBloomFilter(bloomFilterCacheProperties.getName());
+        bloomFilter.tryInit(bloomFilterCacheProperties.getExpectedInsertions(), bloomFilterCacheProperties.getFalseProbability());
+        return bloomFilter;
     }
 }

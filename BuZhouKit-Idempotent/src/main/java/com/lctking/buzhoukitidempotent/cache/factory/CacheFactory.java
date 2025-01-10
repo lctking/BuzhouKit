f10ba456d3490119508dc322c1ce5597eba11bc3
lctking <1713673102lw@gmail.com>
2025-01-11 02:09:10 +0800

    更新：将布隆过滤器缓存服务加入缓存工厂类

--- BuZhouKit-Idempotent/src/main/java/com/lctking/buzhoukitidempotent/cache/factory/CacheFactory.java
+++ BuZhouKit-Idempotent/src/main/java/com/lctking/buzhoukitidempotent/cache/factory/CacheFactory.java
@@ -1,7 +1,9 @@
 package com.lctking.buzhoukitidempotent.cache.factory;
 
+import com.lctking.buzhoukitidempotent.cache.impl.BloomFilterCacheServiceImpl;
 import com.lctking.buzhoukitidempotent.cache.impl.CaffeineCacheServiceImpl;
 import com.lctking.buzhoukitidempotent.cache.impl.RedisCacheServiceImpl;
+import com.lctking.buzhoukitidempotent.cache.service.BloomFilterCacheService;
 import com.lctking.buzhoukitidempotent.cache.service.DistributeCacheService;
 
 import static cn.hutool.extra.spring.SpringUtil.getBean;
@@ -11,10 +13,13 @@ public final class CacheFactory {
 
     private static final String CAFFEINE_CACHE_INITIAL_LOCK = "caffeine_cache_initial_lock";
 
+    private static final String BLOOM_FILTER_CACHE_INITIAL_LOCK = "bloom_filter_cache_initial_lock";
+
     private static CaffeineCacheServiceImpl<String, Object> CAFFEINE_CACHE;
 
     private static final DistributeCacheService<String, Object> REDIS_CACHE = getBean(RedisCacheServiceImpl.class);
 
+    private static BloomFilterCacheService BLOOM_FILTER_CACHE_SERVICE;
 
 
     public static CaffeineCacheServiceImpl<String, Object> getCaffeineCache(){
@@ -28,6 +33,15 @@ public final class CacheFactory {
 
     public static DistributeCacheService<String, Object> getRedisCache(){
         return REDIS_CACHE;
+    }
+
+    public static BloomFilterCacheService getBloomFilter(){
+        if(BLOOM_FILTER_CACHE_SERVICE != null)return BLOOM_FILTER_CACHE_SERVICE;
+        synchronized (BLOOM_FILTER_CACHE_INITIAL_LOCK){
+            if(BLOOM_FILTER_CACHE_SERVICE != null)return BLOOM_FILTER_CACHE_SERVICE;
+            BLOOM_FILTER_CACHE_SERVICE = getBean(BloomFilterCacheServiceImpl.class);
+            return BLOOM_FILTER_CACHE_SERVICE;
+        }
     }
 
 }

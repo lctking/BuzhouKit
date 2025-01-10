806cc54c2400328cfce96c755a6ca1a78a8a83ef
lctking <1713673102lw@gmail.com>
2025-01-11 02:10:11 +0800

    更新：将布隆过滤器幂等执行服务类加入幂等执行工厂类

--- BuZhouKit-Idempotent/src/main/java/com/lctking/buzhoukitidempotent/executor/IdempotentExecuteFactory.java
+++ BuZhouKit-Idempotent/src/main/java/com/lctking/buzhoukitidempotent/executor/IdempotentExecuteFactory.java
@@ -2,6 +2,7 @@ package com.lctking.buzhoukitidempotent.executor;
 
 import com.lctking.buzhoukitidempotent.cache.factory.CacheFactory;
 import com.lctking.buzhoukitidempotent.constant.CacheTypeEnum;
+import com.lctking.buzhoukitidempotent.executor.impl.BloomFilterCacheIdempotentExecuteServiceImpl;
 import com.lctking.buzhoukitidempotent.executor.impl.DistributedCacheIdempotentExecuteServiceImpl;
 import com.lctking.buzhoukitidempotent.executor.impl.LocalCacheIdempotentExecuteServiceImpl;
 import com.lctking.buzhoukitidempotent.executor.service.IdempotentExecuteService;
@@ -10,9 +11,12 @@ public class IdempotentExecuteFactory {
 
     private static final IdempotentExecuteService IDEMPOTENT_EXECUTE_SERVICE_BASED_REDIS;
     private static final IdempotentExecuteService IDEMPOTENT_EXECUTE_SERVICE_BASED_CAFFEINE;
+    private static final IdempotentExecuteService IDEMPOTENT_EXECUTE_SERVICE_BASED_BLOOM_FILTER;
+
     static {
         IDEMPOTENT_EXECUTE_SERVICE_BASED_REDIS = new DistributedCacheIdempotentExecuteServiceImpl(CacheFactory.getRedisCache());
         IDEMPOTENT_EXECUTE_SERVICE_BASED_CAFFEINE = new LocalCacheIdempotentExecuteServiceImpl(CacheFactory.getCaffeineCache());
+        IDEMPOTENT_EXECUTE_SERVICE_BASED_BLOOM_FILTER = new BloomFilterCacheIdempotentExecuteServiceImpl(CacheFactory.getBloomFilter());
     }
 
     public static IdempotentExecuteService getInstance(CacheTypeEnum cacheTypeEnum){
@@ -22,6 +26,9 @@ public class IdempotentExecuteFactory {
             }
             case REDIS -> {
                 return IDEMPOTENT_EXECUTE_SERVICE_BASED_REDIS;
+            }
+            case BLOOM -> {
+                return IDEMPOTENT_EXECUTE_SERVICE_BASED_BLOOM_FILTER;
             }
         }
         return IDEMPOTENT_EXECUTE_SERVICE_BASED_REDIS;
